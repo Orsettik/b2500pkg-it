b6a5ddb30a8311ce52715faef40954682e36fd31
Martin0475 <mschafberger@hotmail.com>
2025-04-21 03:07:36 +0200

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,1 +1,13 @@
-mkdir /config/packages/worked
+github_repo="https://raw.githubusercontent.com/Martin0475/b2500_package/main/packages/b2500_package_by_martin0475"
+local_dir="/config/packages/test/b2500_package_by_martin0475"
+
+for datei in \
+"helpers/b2500_input.yaml" \
+"eintrag2" \
+"eintrag3" \
+"eintrag4" \
+"eintrag5"; do
+    mkdir -p "$datei"
+    rm -f "$datei"
+    wget "$github_repo/$datei" -O "$local_dir/$datei"
+done
