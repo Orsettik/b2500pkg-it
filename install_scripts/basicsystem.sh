131dd3cfba02bd0963a5482491d6b643ebf18ea4
Martin0475 <mschafberger@hotmail.com>
2025-04-25 18:04:34 +0200

    Update basicsystem.sh

--- install_scripts/basicsystem.sh
+++ install_scripts/basicsystem.sh
@@ -4,6 +4,6 @@ mkdir /config/dashboards
 rm - f /config/dashboards/b2500_dashboard.yaml
 sleep 5
 test ! -f "/config/packages/b2500_package/basic_settings.yaml" && wget -q -O /config/packages/b2500_package/basic_settings.yaml https://raw.githubusercontent.com/Martin0475/b2500_package/main/packages/b2500_package/basic_settings.yaml
-wget -q -O /config/dashboards/b2500_dashboard.yaml https://raw.githubusercontent.com/Martin0475/b2500_package/main/dashboards/b25050_dashboard.yaml
+wget -q -O /config/dashboards/b2500_dashboard.yaml https://raw.githubusercontent.com/Martin0475/b2500_package/main/dashboards/b2500_dashboard.yaml
 # Der Automation mitteilen wenn Script abgearbeitet ist
 #curl -X POST https://127.0.0.1/api/webhook/b2500_package_installed
