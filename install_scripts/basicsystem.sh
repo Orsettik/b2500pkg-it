1dc85a5631649fe4545fa4976d6e1d4570a70f29
Martin0475 <mschafberger@hotmail.com>
2025-04-25 19:43:14 +0200

    Add files via upload

--- install_scripts/basicsystem.sh
+++ install_scripts/basicsystem.sh
@@ -1,9 +1,15 @@
 # Befehle hier einfügen
 mkdir /config/packages/b2500_package/automations
+rm -f /config/packages/b2500_package/automations/request_infos.yaml
 mkdir /config/dashboards
 rm - f /config/dashboards/b2500_dashboard.yaml
+
 sleep 5
+
 test ! -f "/config/packages/b2500_package/basic_settings.yaml" && wget -q -O /config/packages/b2500_package/basic_settings.yaml https://raw.githubusercontent.com/Martin0475/b2500_package/main/packages/b2500_package/basic_settings.yaml
+wget -q -O /config/packages/b2500_package/automations/request_infos.yaml https://raw.githubusercontent.com/Martin0475/b2500_package/main/packages/b2500_package/automations/request_infos.yaml
+
+
+
 wget -q -O /config/dashboards/b2500_dashboard.yaml https://raw.githubusercontent.com/Martin0475/b2500_package/main/dashboards/b2500_dashboard.yaml
-# Der Automation mitteilen wenn Script abgearbeitet ist
-#curl -X POST https://127.0.0.1/api/webhook/b2500_package_installed
+
