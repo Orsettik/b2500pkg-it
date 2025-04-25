1a2d463ea770c157ba565056206b6142bd5570a9
Martin0475 <mschafberger@hotmail.com>
2025-04-25 15:05:13 +0200

    Update basicsystem.sh

--- install_scripts/basicsystem.sh
+++ install_scripts/basicsystem.sh
@@ -1,4 +1,8 @@
 # Befehle hier einfügen
+mkdir /config/packages/b2500_package/automations
+mkdir /config/packages/b2500_package/settings
+sleep 5
+wget -q -O /config/packages/b2500_package/automations/b01_sensors_basic.yaml https://raw.githubusercontent.com/Martin0475/b2500_package/main/automations/b01_sensors_basic.yaml
 
 
 # Der Automation mitteilen wenn Script abgearbeitet ist
