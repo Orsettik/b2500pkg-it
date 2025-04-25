08e4eb441cf16865ab8300cd83a59e3471d98459
Martin0475 <mschafberger@hotmail.com>
2025-04-25 14:29:42 +0200

    Update basicsystem.sh

--- install_scripts/basicsystem.sh
+++ install_scripts/basicsystem.sh
@@ -2,4 +2,4 @@
 
 
 # Der Automation mitteilen wenn Script abgearbeitet ist
-curl -X POST https://dein-ha-url.de/api/webhook/b2500_package_installed
+curl -X POST https://127.0.0.1/api/webhook/b2500_package_installed
