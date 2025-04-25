5a5fa66bac48d6a863ea34f53b68e71fdffc434a
Martin0475 <mschafberger@hotmail.com>
2025-04-25 20:29:48 +0200

    Add files via upload

--- install_scripts/basicsystem.sh
+++ install_scripts/basicsystem.sh
@@ -1,6 +1,14 @@
 # Befehle hier einfügen
 mkdir /config/packages/b2500_package/automations
 rm -f /config/packages/b2500_package/automations/request_infos.yaml
+rm -f /config/packages/b2500_package/automations/repost_cd01_cd13.yaml
+
+
+
+
+
+
+
 mkdir /config/dashboards
 rm - f /config/dashboards/b2500_dashboard.yaml
 
@@ -8,6 +16,7 @@ sleep 5
 
 test ! -f "/config/packages/b2500_package/basic_settings.yaml" && wget -q -O /config/packages/b2500_package/basic_settings.yaml https://raw.githubusercontent.com/Martin0475/b2500_package/main/packages/b2500_package/basic_settings.yaml
 wget -q -O /config/packages/b2500_package/automations/request_infos.yaml https://raw.githubusercontent.com/Martin0475/b2500_package/main/packages/b2500_package/automations/request_infos.yaml
+wget -q -O /config/packages/b2500_package/automations/repost_cd01_cd13.yaml https://raw.githubusercontent.com/Martin0475/b2500_package/main/packages/b2500_package/automations/repost_cd01_cd13.yaml
 
 
 
