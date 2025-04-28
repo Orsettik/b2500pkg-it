5f9264940462fb0018a849cc4c383537b5801e16
Martin0475 <mschafberger@hotmail.com>
2025-04-28 11:53:11 +0200

    Update basicsystem.sh

--- install_scripts/basicsystem.sh
+++ install_scripts/basicsystem.sh
@@ -1,7 +1,8 @@
 # Befehle hier einfügen
-mkdir /config/packages/b2500_package/automations
-rm -f /config/packages/b2500_package/automations/request_infos.yaml
-rm -f /config/packages/b2500_package/automations/repost_cd01_cd13.yaml
+mkdir /config/packages/b2500pkg
+mkdir /config/packages/b2500pkg/automations
+rm -f /config/packages/b2500pkg/automations/request_infos.yaml
+rm -f /config/packages/b2500pkg/automations/repost_cd01_cd13.yaml
 
 
 
@@ -10,15 +11,15 @@ rm -f /config/packages/b2500_package/automations/repost_cd01_cd13.yaml
 
 
 mkdir /config/dashboards
-rm - f /config/dashboards/b2500_dashboard.yaml
+rm - f /config/dashboards/b2500pkg_dashboard.yaml
 
 sleep 5
 
-test ! -f "/config/packages/b2500_package/basic_settings.yaml" && wget -q -O /config/packages/b2500_package/basic_settings.yaml https://raw.githubusercontent.com/Martin0475/b2500_package/main/packages/b2500_package/basic_settings.yaml
-wget -q -O /config/packages/b2500_package/automations/request_infos.yaml https://raw.githubusercontent.com/Martin0475/b2500_package/main/packages/b2500_package/automations/request_infos.yaml
-wget -q -O /config/packages/b2500_package/automations/repost_cd01_cd13.yaml https://raw.githubusercontent.com/Martin0475/b2500_package/main/packages/b2500_package/automations/repost_cd01_cd13.yaml
+test ! -f "/config/packages/b2500pkg/basic_settings.yaml" && wget -q -O /config/packages/b2500pkg/basic_settings.yaml https://raw.githubusercontent.com/Martin0475/b2500pkg/main/packages/b2500pkg/basic_settings.yaml
+wget -q -O /config/packages/b2500pkg/automations/request_infos.yaml https://raw.githubusercontent.com/Martin0475/b2500pkg/main/packages/b2500pkg/automations/request_infos.yaml
+wget -q -O /config/packages/b2500pkg/automations/repost_cd01_cd13.yaml https://raw.githubusercontent.com/Martin0475/b2500pkg/main/packages/b2500pkg/automations/repost_cd01_cd13.yaml
 
 
 
-wget -q -O /config/dashboards/b2500_dashboard.yaml https://raw.githubusercontent.com/Martin0475/b2500_package/main/dashboards/b2500_dashboard.yaml
+wget -q -O /config/dashboards/b2500pkg_dashboard.yaml https://raw.githubusercontent.com/Martin0475/b2500pkg/main/dashboards/b2500pkg_dashboard.yaml
 
