c43e0e788027ca177b84356a87a512c33adeb76a
Martin0475 <mschafberger@hotmail.com>
2025-04-28 23:05:08 +0200

    Update basicsystem.sh

--- install_scripts/basicsystem.sh
+++ install_scripts/basicsystem.sh
@@ -3,7 +3,10 @@ mkdir /config/packages/b2500pkg
 mkdir /config/packages/b2500pkg/automations
 rm -f /config/packages/b2500pkg/automations/request_infos.yaml
 rm -f /config/packages/b2500pkg/automations/repost_cd01_cd13.yaml
-
+rm -f /config/packages/b2500pkg/automations/b01_sensor_discovery.yaml
+rm -f /config/packages/b2500pkg/automations/b02_sensor_discovery.yaml
+rm -f /config/packages/b2500pkg/automations/b03_sensor_discovery.yaml
+rm -f /config/packages/b2500pkg/automations/b04_sensor_discovery.yaml
 
 
 
@@ -18,6 +21,10 @@ sleep 5
 test ! -f "/config/packages/b2500pkg/basic_settings.yaml" && wget -q -O /config/packages/b2500pkg/basic_settings.yaml https://raw.githubusercontent.com/Martin0475/b2500pkg/main/packages/b2500pkg/basic_settings.yaml
 wget -q -O /config/packages/b2500pkg/automations/request_infos.yaml https://raw.githubusercontent.com/Martin0475/b2500pkg/main/packages/b2500pkg/automations/request_infos.yaml
 wget -q -O /config/packages/b2500pkg/automations/repost_cd01_cd13.yaml https://raw.githubusercontent.com/Martin0475/b2500pkg/main/packages/b2500pkg/automations/repost_cd01_cd13.yaml
+wget -q -O /config/packages/b2500pkg/automations/b01_sensor_discovery.yaml https://raw.githubusercontent.com/Martin0475/b2500pkg/main/packages/b2500pkg/automations/b01_sensor_discovery.yaml
+wget -q -O /config/packages/b2500pkg/automations/b02_sensor_discovery.yaml https://raw.githubusercontent.com/Martin0475/b2500pkg/main/packages/b2500pkg/automations/b02_sensor_discovery.yaml
+wget -q -O /config/packages/b2500pkg/automations/b03_sensor_discovery.yaml https://raw.githubusercontent.com/Martin0475/b2500pkg/main/packages/b2500pkg/automations/b03_sensor_discovery.yaml
+wget -q -O /config/packages/b2500pkg/automations/b04_sensor_discovery.yaml https://raw.githubusercontent.com/Martin0475/b2500pkg/main/packages/b2500pkg/automations/b04_sensor_discovery.yaml
 
 
 
