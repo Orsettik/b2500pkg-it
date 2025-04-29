0fbcb2f0e1aee42eafd81ab9e4a6709375985921
Martin0475 <mschafberger@hotmail.com>
2025-04-29 21:51:08 +0200

    Update basicsystem.sh

--- install_scripts/basicsystem.sh
+++ install_scripts/basicsystem.sh
@@ -1,6 +1,7 @@
 # Befehle hier einfügen
 mkdir /config/packages/b2500pkg
 mkdir /config/packages/b2500pkg/automations
+
 rm -f /config/packages/b2500pkg/automations/request_infos.yaml
 rm -f /config/packages/b2500pkg/automations/repost_cd01_cd13.yaml
 rm -f /config/packages/b2500pkg/automations/b01_sensor_discovery.yaml
@@ -8,10 +9,7 @@ rm -f /config/packages/b2500pkg/automations/b02_sensor_discovery.yaml
 rm -f /config/packages/b2500pkg/automations/b03_sensor_discovery.yaml
 rm -f /config/packages/b2500pkg/automations/b04_sensor_discovery.yaml
 
-
-
-
-
+rm -f /config/packages/b2500pkg/b2500pkg_input.yaml
 
 mkdir /config/dashboards
 rm - f /config/dashboards/b2500pkg_dashboard.yaml
@@ -25,6 +23,8 @@ wget -q -O /config/packages/b2500pkg/automations/b01_sensor_discovery.yaml https
 wget -q -O /config/packages/b2500pkg/automations/b02_sensor_discovery.yaml https://raw.githubusercontent.com/Martin0475/b2500pkg/main/packages/b2500pkg/automations/b02_sensor_discovery.yaml
 wget -q -O /config/packages/b2500pkg/automations/b03_sensor_discovery.yaml https://raw.githubusercontent.com/Martin0475/b2500pkg/main/packages/b2500pkg/automations/b03_sensor_discovery.yaml
 wget -q -O /config/packages/b2500pkg/automations/b04_sensor_discovery.yaml https://raw.githubusercontent.com/Martin0475/b2500pkg/main/packages/b2500pkg/automations/b04_sensor_discovery.yaml
+
+wget -q -O /config/packages/b2500pkg/b2500pkg_input.yaml https://raw.githubusercontent.com/Martin0475/b2500pkg/main/packages/b2500pkg/b2500pkg_input.yaml
 
 
 
